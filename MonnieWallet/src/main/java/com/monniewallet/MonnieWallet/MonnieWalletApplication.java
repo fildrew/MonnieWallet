336d705b6b710cfaf632fcb267e3965b12c3be13
fildrew <philedu30@gmail.com>
2025-02-17 22:58:48 +0100

    Initial commit

---
+++ MonnieWallet/src/main/java/com/monniewallet/MonnieWallet/MonnieWalletApplication.java
@@ -0,0 +1,13 @@
+package com.monniewallet.MonnieWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonnieWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonnieWalletApplication.class, args);
+	}
+
+}
---
+++ MonnieWallet/src/test/java/com/monniewallet/MonnieWallet/MonnieWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monniewallet.MonnieWallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonnieWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
